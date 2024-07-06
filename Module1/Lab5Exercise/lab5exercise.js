cad7ee33d5915c16a11dc6fc7f6ff123eb345930
John Carlos <adrcarlos02@gmail.com>
2024-07-06 23:50:14 +1000

    lab 5 continued

---
+++ Module1/Lab5Exercise/lab5exercise.js
@@ -0,0 +1,16 @@
+// Define an array
+
+let planets = ['Venus', 'Pluto', 'Mars', 'Jupiter', 'Neptune'];
+
+//Replace value of Array
+planets[1] = 'Earth';
+planets[4] = 'Saturn';
+
+//Add a new element at the beginning of the Array
+planets.unshift('Mercury');
+
+//Remove last element at the end of the Array
+planets.pop();
+
+//Print the Array in the console
+console.log(planets);
