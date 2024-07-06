4108559738f25021e0ceb18d57a95559a34531dd
John Carlos <adrcarlos02@gmail.com>
2024-07-07 09:18:03 +1000

    Lab6 completed

---
+++ Module1/Lab6Exercise/lab6Exercise.js
@@ -0,0 +1,126 @@
+//creating a json variable of a book
+//1st
+const book1 = {
+  title: "A Game of Thrones",
+  description: "The first book of the World of Ice and Fire Series",
+  author: "George R. R. Martin",
+  numberOfPages: "694"
+};
+
+//Logging the book object to the console
+console.log(book1);
+
+//Accessing properties
+console.log("Title:", book1.title);
+console.log("Description:", book1.description);
+console.log("Author:", book1.author)
+console.log("Number of Pages:", book1.numberOfPages);
+
+//Modifying book1 descriptoon property
+book1.description = "The novel is a work of epic fantasy that follows the political machinations of several noble families as they vie for control of the Iron Throne of the land of Westeros"; //Modififying  the number of pages 
+
+//logging the updated description to the console
+console.log("Description:", book1.description);
+
+
+//2nd
+const book2 = {
+  title: "A Clash of Kings",
+  description: "A Clash of Kings depicts the Seven Kingdoms of Westeros in civil war, while the Night's Watch mounts a reconnaissance to investigate the mysterious people known as wildlings",
+  author: "George R. R. Martin",
+  numberOfPages: "768"
+};
+
+//Logging the book object to the console
+console.log(book2);
+
+//Accessing properties
+console.log("Title:", book2.title);
+console.log("Description:", book2.description);
+console.log("Author:", book2.author)
+console.log("Number of Pages:", book2.numberOfPages);
+
+//3rd
+const book3 = {
+  title: "A Storm of Swords",
+  description: "A Storm of Swords continues the story where A Clash of Kings ended. The novel describes the increasingly vicious War of Five Kings in Westeros, Daenerys's strengthening forces in the East, and the oncoming threat of the Others, a ghostly army that is nearly invincible.",
+  author: "George R. R. Martin",
+  numberOfPages: "973"
+};
+
+//Logging the book object to the console
+console.log(book3);
+
+//Accessing properties
+console.log("Title:", book3.title);
+console.log("Description:", book3.description);
+console.log("Author:", book3.author)
+console.log("Number of Pages:", book3.numberOfPages);
+
+//4th
+const book4 = {
+  title: "A Feast for Crows",
+  description: "A Feast for Crows focuses on the Lannister family's continuing consolidation of power following victory in the “War of the Five Kings.” Specifically, it follows the events precipitated by the murder of Tywin Lannister, who had been de facto ruler of Westeros. In his place, his daughter Cersei, seizes power.",
+  author: "George R. R. Martin",
+  numberOfPages: "753"
+};
+
+//Logging the book object to the console
+console.log(book4);
+
+//Accessing properties
+console.log("Title:", book4.title);
+console.log("Description:", book4.description);
+console.log("Author:", book4.author)
+console.log("Number of Pages:", book4.numberOfPages);
+
+//5th
+const book5 = {
+  title: "A Dance with Dragons",
+  description: "In the aftermath of a colossal battle, the future of the Seven Kingdoms hangs in the balance once again - beset by newly emerging threats from every direction. In the east, Daenerys Targaryen, the last scion of House Targaryen, rules with her three dragons as queen of a city built on dust and death.",
+  author: "George R. R. Martin",
+  numberOfPages: "1056"
+};
+
+//Logging the book object to the console
+console.log(book5);
+
+//Accessing properties
+console.log("Title:", book5.title);
+console.log("Description:", book5.description);
+console.log("Author:", book5.author)
+console.log("Number of Pages:", book5.numberOfPages);
+
+//6th
+const book6 = {
+  title: "The Winds of Winter",
+  description: "The Winds of Winter tells the story of Dany's fight to save her new-won kingdom, of two desperate journeys beyond the known world in to the very hearts of ice and fire, and of the final clomactic battle at Winterfell, with life itself in the balance.",
+  author: "George R. R. Martin",
+  numberOfPages: "Forthcoming"
+};
+
+//Logging the book object to the console
+console.log(book6);
+
+//Accessing properties
+console.log("Title:", book6.title);
+console.log("Description:", book6.description);
+console.log("Author:", book6.author)
+console.log("Number of Pages:", book6.numberOfPages);
+
+//7th
+const book7 = {
+  title: "A Dream of Spring",
+  description: "Forthcoming",
+  author: "George R. R. Martin",
+  numberOfPages: "Forthcoming"
+};
+
+//Logging the book object to the console
+console.log(book7);
+
+//Accessing properties
+console.log("Title:", book7.title);
+console.log("Description:", book7.description);
+console.log("Author:", book7.author)
+console.log("Number of Pages:", book7.numberOfPages);
