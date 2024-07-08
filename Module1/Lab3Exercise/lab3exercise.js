aaa87e259aaf0ade597b2fc218d3c754b1bcf2e8
John Carlos <adrcarlos02@gmail.com>
2024-07-08 16:09:11 +0800

    Lab 3 Exercise completed

--- Module1/Lab3Exercise/lab3exercise.js
+++ Module1/Lab3Exercise/lab3exercise.js
@@ -6,13 +6,26 @@
         const img = document.createElement('img');
         img.src = 'iceandfire/Ice5.jpg'; // will replace with your image path
         img.alt = 'Ice Photo';
-        img.style.maxWidth = '100%'; // 
+        img.style.maxWidth = '100%'; 
         container.appendChild(img);
+
+        const column1 = document.getElementById('column1');
+        column1.style.backgroundColor = 'rgb(52, 78, 111)';
+        
+        const changetext1 =  document.getElementById('heading1');
+         changetext1.innerText = 'ICE';
+        
     });
 
-    function changeColor1() {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16); // 
-    document.body.style.backgroundColor = 'rgb(88, 121, 141)';
+    
+
+    function changetext1() {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16); // 
+         document.body.style.backgroundColor = 'rgb(88, 121, 141)';
+
+         
+
+
     }
 
 
@@ -27,9 +40,17 @@
       img.alt = 'Fire Photo';
       img.style.maxWidth = '100%'; // 
       container.appendChild(img);
+
+      const column2 = document.getElementById('column2')
+      column2.style.backgroundColor = 'rgb(210, 85, 17)'
+     
+      const changetext1 =  document.getElementById('heading2');
+      changetext1.innerText = 'Fire';
   });
 
-  function changeColor2() {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16); 
-  document.body.style.backgroundColor = 'rgb(180, 69, 65)';
-  }
+    function changetext2() {
+        const randomColor = Math.floor(Math.random()*16777215).toString(16); 
+         document.body.style.backgroundColor = 'rgb(180, 69, 65)';
+
+      
+    }
